5ea7967916f0e9526c498c72b9c6879d152dc0d6
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-12-04 22:48:16 +0700

    arch

--- controller/SpotifyController.go
+++ controller/SpotifyController.go
@@ -7,13 +7,9 @@ import (
 )
 
 func SpotifyController(response http.ResponseWriter, request *http.Request) {
-	method := request.Method
-	if method != "get" {
-		response.Write([]byte("Error"))
-	}
-
 	repo := data.InitSpotifyRepository()
 
 	result := repo.GetSpotifyCard()
+
 	response.Write([]byte(result))
 }
